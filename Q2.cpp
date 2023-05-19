78a0056938173e5b21bb293b8caad0b75f1d66cf
Surbhi Goel <surbhigoel02@gmail.com>
2023-05-19 16:33:26 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,3 +1,12 @@
+//HASHING QUESTION NO. 2
+
+//program to find two sum
+
+//use unordered_map <int,int>, instead of frequency add index to the map mp[nums[i]] = i
+// Time complexity : O(n)
+// Space complexity : O(n)
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
